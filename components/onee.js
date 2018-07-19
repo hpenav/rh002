8d000c1335e17c985d1bca0f0e754604a091e857
hpenav <domotica.humberto@gmail.com>
2018-07-18 21:34:55 -0500

    dev

---
+++ components/onee.js
@@ -0,0 +1,29 @@
+var noflo = require('noflo');
+
+exports.getComponent = function() {
+  var c = new noflo.Component();
+  c.description = 'onee';
+  c.icon = 'sort-numeric-asc';
+  c.inPorts.add('in', {
+    datatype: 'all',
+    description: 'Packet to forward'
+  });
+  c.outPorts.add('out', {
+    datatype: 'all'
+  });
+  c.process(function (input, output) {
+    // Check preconditions on input data
+    if (!input.hasData('in')) {
+      return;
+    }
+    // Read packets we need to process
+    var data = input.getData('in');
+    // Process data and send output
+    output.send({
+      out: data
+    });
+    // Deactivate
+    output.done();
+  });
+  return c;
+};
